89c7d2307960a367a5b52267a94493c62703dc92
Ansh kumain <104789216+vector17002@users.noreply.github.com>
2024-03-11 15:19:55 +0530

    Update page.jsx

--- app/api/profile/page.jsx
+++ app/api/profile/page.jsx
@@ -14,7 +14,7 @@ const Wishes = ({ anime, change, modal, detail, idx }) => {
     <div onClick={handleModal}
       className='p-2 m-2 rounded-xl flex flex-col justify-between items-center border-amber-300 border-2 cursor-pointer hover:scale-105 relative'>
       <div>
-        <img src={anime.animeImg} alt='img' className='lg:h-[300px] h-[250px] min-w-[150px] lg:min-w-[200px] rounded-lg' />
+        <img src={anime.animeImg} alt='img' className='lg:h-[300px] h-[250px] min-w-[175px] lg:min-w-[200px] rounded-lg' />
       </div>
       <div>
         <p className='desc'><span className='orange_gradient font-extrabold'>{idx + 1}. </span>{anime.title_english ? anime.animeName.substr(0, 15) : anime.animeName.substr(0, 15)}..</p>
